ecba509b94933820107ccd0511c7e52c6f7d89ff
Kadyrov Rushat <kadirovgm@gmail.com>
2020-10-30 16:10:31 +0300

    30.10

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "AVL.h"
+#include "STD.h"
 
 using namespace std;
 using namespace avl;
@@ -7,19 +8,30 @@ using namespace atl;
 
 int main()
 {
-    avl::Image image_in;
-    avl::Image image_wb;
-    avl::Image image_thresh;
+    avl::Image image_in; //input
+    avl::Image image_wb; //wb
+    avl::Image image_thresh; //after thresholding
+    avl::Image image_smooth; //after gauss smooth
+
+    //Kernel method
+    //In machine learning, kernel methods are a class of algorithms for pattern analysis,
+    //whose best known member is the support vector machine (SVM).
+    int diagKernelRadiusX = 1;
+    int diagKernelRadiusY = 1;
+
     //load image in.jpg
     avl::LoadImage("in.jpg", 0, image_in);
     //Creates a monochromatic image by averaging the input image channels.
     avl::AverageChannels(image_in, atl::NIL, image_wb);
-    //save wb image to jpg
     avl::SaveImageToJpeg(image_wb, "out_wb.jpg", 100, 0);
+
     //Transforms each pixel value to maximum or minimum depending on whether they belong to the specified range
-    avl::ThresholdImage(image_wb, atl::NIL, 200.0f, atl::NIL, 50.0f, image_thresh); //
-    //save threshhold image to jpg
+    avl::ThresholdImage(image_wb, atl::NIL, 200.0f, atl::NIL, 50.0f, image_thresh);
     avl::SaveImageToJpeg(image_thresh, "out_thresh.jpg", 100, 0);
+
+    //Gauss smooth
+    avl::SmoothImage_Gauss(image_thresh, atl::NIL, 1.2f, atl::NIL, 2.0f, image_smooth, diagKernelRadiusX,diagKernelRadiusY );
+    avl::SaveImageToJpeg(image_smooth, "out_smooth.jpg", 100, 0);
 
     return 0;
 }
