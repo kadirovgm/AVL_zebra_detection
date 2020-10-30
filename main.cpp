3259877aa7c02479610441789cd02e6fa533da73
Kadyrov Rushat <kadirovgm@gmail.com>
2020-10-30 14:56:14 +0300

    new

--- main.cpp
+++ main.cpp
@@ -10,10 +10,15 @@ int main()
     avl::Image image_in;
     avl::Image image_wb;
     avl::Image image_thresh;
-    avl::LoadImage("in2.jpg", 0, image_in);
+    //load image in.jpg
+    avl::LoadImage("in.jpg", 0, image_in);
+    //Creates a monochromatic image by averaging the input image channels.
     avl::AverageChannels(image_in, atl::NIL, image_wb);
+    //save wb image to jpg
     avl::SaveImageToJpeg(image_wb, "out_wb.jpg", 100, 0);
-    avl::ThresholdImage(image_wb, atl::NIL, 120.0f, atl::NIL, 0.0f, image_thresh);
+    //Transforms each pixel value to maximum or minimum depending on whether they belong to the specified range
+    avl::ThresholdImage(image_wb, atl::NIL, 200.0f, atl::NIL, 50.0f, image_thresh); //
+    //save threshhold image to jpg
     avl::SaveImageToJpeg(image_thresh, "out_thresh.jpg", 100, 0);
 
     return 0;
