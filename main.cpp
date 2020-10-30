c2cf7a45d5730b398cf14d8f8dc2290f8ca20352
Kadyrov Rushat <kadirovgm@gmail.com>
2020-10-30 14:37:10 +0300

    smth new

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include "AVL.h"
 
+using namespace std;
+using namespace avl;
+using namespace atl;
+
 int main()
 {
-    avl::Point2D a(0, 0);
-    avl::Point2D b(1, 1);
-    float distance;
-
-    try
-    {
-        avl::PointToPointDistance(a, b, 1.0f, distance, atl::NIL);
-    }
-    catch(const atl::Error& e)
-    {
-        std::cout << e.Kind() << ": " << e.Message() << std::endl;
-        return 1;
-    }
-
-    std::cout << "Distance: " << distance << std::endl;
+    avl::Image image_in;
+    avl::Image image_wb;
+    avl::Image image_thresh;
+    avl::LoadImage("in2.jpg", 0, image_in);
+    avl::AverageChannels(image_in, atl::NIL, image_wb);
+    avl::SaveImageToJpeg(image_wb, "out_wb.jpg", 100, 0);
+    avl::ThresholdImage(image_wb, atl::NIL, 120.0f, atl::NIL, 0.0f, image_thresh);
+    avl::SaveImageToJpeg(image_thresh, "out_thresh.jpg", 100, 0);
 
     return 0;
 }
+
